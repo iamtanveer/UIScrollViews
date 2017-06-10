c346696a758bf8005152039cfd233304028ac2f1
Tanveer Singh <tanveer7537@gmail.com>
2017-06-11 00:25:53 +0530

    Final app ready

--- PageTheScroll/ViewController.swift
+++ PageTheScroll/ViewController.swift
@@ -9,17 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var scrollView: UIScrollView!
+    var images = [UIImageView]() //an empty array of Image views
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        
+        let scrollWidth = scrollView.frame.size.width
+        for x in 0...2{
+            let image = UIImage(named: "icon\(x)")
+            let imageView = UIImageView(image:image) // adding images to the UIImageView array
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x) //to find out the mid point of all the pages
+            contentWidth += newX
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 75, y: (scrollView.frame.size.height / 2) - 75, width: 150, height: 150)
+            // added offset to y to adjust to centre of UIimageview
+            
+        }
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        scrollView.clipsToBounds = false
+        
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
 }
 
